cc4da448dfae48a8b9060d840ca289436a2c3ef6
Deepak Tiwari <deeptiwari690@gmail.com>
2026-06-14 17:27:24 +0530

    feat: add movie summary and list schemas

---
+++ src/features/movies/schemas.ts
@@ -0,0 +1,17 @@
+import { z } from "zod";
+
+export const movieSummarySchema = z.object({
+  id: z.number(),
+  title: z.string(),
+  poster_path: z.string().nullable(),
+  release_date: z.string(),
+  vote_average: z.number(),
+  genre_ids: z.array(z.number()),
+});
+
+export const movieListSchema = z.object({
+  page: z.number(),
+  results: z.array(movieSummarySchema),
+  total_pages: z.number(),
+  total_results: z.number(),
+});

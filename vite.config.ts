2cfa504c85280f324ab4949e2eeaa1a9ea00c863
Deepak Tiwari <deeptiwari690@gmail.com>
2026-06-14 18:04:39 +0530

    chore: add @/ path alias

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 import tailwindcss from "@tailwindcss/vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [react(), svgr(), tailwindcss()],
+  plugins: [react(), svgr(), tailwindcss(), tsconfigPaths()],
 });

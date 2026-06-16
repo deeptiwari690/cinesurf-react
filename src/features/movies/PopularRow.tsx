76af32b5b71859432c8d120f3635d49962611e31
Deepak Tiwari <deeptiwari690@gmail.com>
2026-06-16 16:24:53 +0530

    fix: switch Popular row carousel to slidesPerGroupAuto snap behavior

--- src/features/movies/PopularRow.tsx
+++ src/features/movies/PopularRow.tsx
@@ -1,7 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import { popularMoviesQuery } from "./queries";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { FreeMode } from "swiper/modules";
 import { MovieCard } from "./MovieCard";
 
 export function PopularRow() {
@@ -13,7 +12,7 @@ export function PopularRow() {
   return (
     <section>
       <h2 className="mb-4 text-xl font-bold">What's Popular</h2>
-      <Swiper slidesPerView="auto" grabCursor className="select-none" modules={[FreeMode]} freeMode>
+      <Swiper slidesPerView="auto" grabCursor slidesPerGroupAuto speed={500} className="select-none">
         {data.results.map((movie) => (
           <SwiperSlide key={movie.id} className="w-44 pr-4 last:w-40 last:pr-0">
             <MovieCard movie={movie} />

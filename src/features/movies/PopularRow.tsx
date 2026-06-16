dc9046e2aef8cd60fbf6848fe7cb080a2fac0a3c
Deepak Tiwari <deeptiwari690@gmail.com>
2026-06-16 16:39:14 +0530

    fix: switch Popular row to freeMode with controlled momentumRatio

--- src/features/movies/PopularRow.tsx
+++ src/features/movies/PopularRow.tsx
@@ -1,6 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import { popularMoviesQuery } from "./queries";
 import { Swiper, SwiperSlide } from "swiper/react";
+import { FreeMode } from "swiper/modules";
 import { MovieCard } from "./MovieCard";
 
 export function PopularRow() {
@@ -12,7 +13,13 @@ export function PopularRow() {
   return (
     <section>
       <h2 className="mb-4 text-xl font-bold">What's Popular</h2>
-      <Swiper slidesPerView="auto" grabCursor slidesPerGroupAuto speed={500} className="select-none">
+      <Swiper
+        slidesPerView="auto"
+        grabCursor
+        className="select-none"
+        modules={[FreeMode]}
+        freeMode={{ enabled: true, momentumRatio: 0.5 }}
+      >
         {data.results.map((movie) => (
           <SwiperSlide key={movie.id} className="w-44 pr-4 last:w-40 last:pr-0">
             <MovieCard movie={movie} />

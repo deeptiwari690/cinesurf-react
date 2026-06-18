bb326bddf60578eb1992c5f473362b6f7dff6202
Deepak Tiwari <deeptiwari690@gmail.com>
2026-06-18 21:17:25 +0530

    refactor: align query key namespace with TMDB path segment (movies -> movie)

--- src/features/movies/queries.ts
+++ src/features/movies/queries.ts
@@ -3,12 +3,12 @@ import { tmdb } from "@/lib/tmdb";
 import { movieListSchema } from "./schemas";
 
 export const popularMoviesQuery = queryOptions({
-  queryKey: ["movies", "popular"],
+  queryKey: ["movie", "popular"],
   queryFn: ({ signal }) => tmdb("/movie/popular", movieListSchema, signal),
 });
 
 export const searchMoviesQuery = (query: string, page: number) =>
   queryOptions({
-    queryKey: ["movies", "search", query, page],
+    queryKey: ["movie", "search", query, page],
     queryFn: ({ signal }) => tmdb("/search/movie", movieListSchema, signal, { query, page: String(page) }),
   });

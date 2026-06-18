bec067698d3783d45401ec220baa9c9b36f4791e
Deepak Tiwari <deeptiwari690@gmail.com>
2026-06-19 02:39:40 +0530

    refactor: lift errorElement to LandingLayout and wire homeLoader to index route

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router";
 import { LandingLayout } from "./LandingLayout";
 import { Home } from "./Home";
+import { homeLoader } from "./Home.loader";
 import { SearchResults } from "./SearchResults";
 import { searchLoader } from "./SearchResults.loader";
 import { ErrorPage } from "@/components/ErrorPage";
@@ -8,13 +9,17 @@ import { ErrorPage } from "@/components/ErrorPage";
 export const router = createBrowserRouter([
   {
     element: <LandingLayout />,
+    errorElement: <ErrorPage />,
     children: [
-      { index: true, element: <Home /> },
+      {
+        index: true,
+        element: <Home />,
+        loader: homeLoader,
+      },
       {
         path: "search",
         element: <SearchResults />,
         loader: searchLoader,
-        errorElement: <ErrorPage />,
       },
     ],
   },

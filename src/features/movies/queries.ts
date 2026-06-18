e87dffd5e0f7ef79aed18c7363e8ea2b198a6138
Deepak Tiwari <deeptiwari690@gmail.com>
2026-06-18 16:26:07 +0530

    refactor: centralize staleTime and gcTime defaults in QueryClient

--- src/features/movies/queries.ts
+++ src/features/movies/queries.ts
@@ -5,12 +5,10 @@ import { movieListSchema } from "./schemas";
 export const popularMoviesQuery = queryOptions({
   queryKey: ["movies", "popular"],
   queryFn: ({ signal }) => tmdb("/movie/popular", movieListSchema, undefined, signal),
-  staleTime: 1000 * 60 * 5,
 });
 
 export const searchMoviesQuery = (query: string, page: number) =>
   queryOptions({
     queryKey: ["movies", "search", query, page],
     queryFn: ({ signal }) => tmdb("/search/movie", movieListSchema, { query, page: String(page) }, signal),
-    staleTime: 1000 * 60 * 5,
   });
